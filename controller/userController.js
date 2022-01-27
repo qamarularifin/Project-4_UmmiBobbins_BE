e25bea76511364488fa65ba8bd7b52647d34d6ba
qamarularifin <qamarulafro89@gmail.com>
2022-01-27 17:59:15 +0800

    created basic backend files

---
+++ controller/userController.js
@@ -0,0 +1,145 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const User = require("../models/userModel")
+const jwt = require("jsonwebtoken")
+const SECRET = process.env.SECRET
+
+
+// get all users
+router.get("/", async (req, res) => {
+    let users;
+    try {
+      users = await User.find({});
+    } catch (error) {
+      res.status(500).send({ message: "Unexpected Error" });
+      return;
+    }
+    //res.json({status: "ok"})
+    res.send(users);
+  });
+
+
+router.post("/register", async(req,res)=>{
+ 
+    try {
+        const newPassword = await bcrypt.hash(req.body.password, 10)
+        await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: newPassword
+        });
+        res.json({status: "ok"})
+        //res.send(user)
+    } catch (error) {
+        res.json({status: "error", error: "Duplicate email"})
+        
+    }   
+})
+
+router.post("/login", async(req,res)=>{
+   
+        const user = await User.findOne({
+            email: req.body.email,
+            //password: req.body.password
+        })
+
+        if (!user){
+            return res.json({status: "error", error: "Invalid login"})
+        }
+
+        const isPasswordValid = await bcrypt.compare(req.body.password, user.password)
+
+        if (isPasswordValid) {
+
+            const token = jwt.sign({
+                name: user.name,
+                email: user.email
+            }, SECRET)
+            //res.json is to show in network. Note: go to Preview
+            return res.json(
+                {status: "ok", user: token}
+            )
+        } else{
+            return res.json(
+                {status: "error", user: false}
+            )
+        }
+ 
+  
+       
+})
+
+
+// use this get request to render (permanent) fields in the dashboard
+router.get("/quote", async(req,res)=>{
+   
+    const token = req.headers["x-access-token"]
+
+    try{
+        const decoded = jwt.verify(token, SECRET) // authenticate token
+        const email = decoded.email
+        const user = await User.findOne({email: email})
+        return res.json({status: "ok", 
+                         quote: user.quote, 
+                         email: user.email,
+                         name: user.name,
+                         _id: user._id}) // get the quote based on the user email //quote will be exclusive to profile 
+    } catch(error){
+        console.log(error)
+        res.json({status: "error", error: "invalid token"})
+    }
+    
+})
+
+
+router.post("/quote", async(req,res)=>{
+   
+    const token = req.headers["x-access-token"]
+
+    try{
+        const decoded = jwt.verify(token, SECRET)  // authenticate token
+        const email = decoded.email
+        await User.updateOne(
+            {email: email},
+            {$set: {quote: req.body.quote}}
+            )
+        return res.json({status: "ok"}) // get the quote based on the user email
+    } catch(error){
+        console.log(error)
+        res.json({status: "error", error: "invalid token"})
+    }
+    
+})
+
+// get userinfo
+// router.get("/:id", async (req, res) => {
+//     const { id } = req.params;
+//     const findUser = await User.findById(id)
+//     // returns object with username, password and role
+//     res.send(findUser)
+//   });
+
+
+//   router.get("/:id", async(req,res)=>{
+   
+//     const token = req.headers["x-access-token"]
+
+//     try{
+//         const decoded = jwt.verify(token, SECRET) // authenticate token
+//         const email = decoded.email
+//         const user = await User.findOne({email: email})
+//         return res.json({status: "ok", _id: user._id}) // get the quote based on the user email //quote will be exclusive to profile 
+//     } catch(error){
+//         console.log(error)
+//         res.json({status: "error", error: "invalid token"})
+//     }
+    
+// })
+
+
+
+
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose')
+const cors = require("cors")
+require('dotenv').config();
+
+
+
+app.use(cors())
+app.use(express.json());
+
+
+// const
+const PORT=process.env.PORT;
+const DATABASE=process.env.DATABASE;
+const MONGO_USER=process.env.MONGO_USER;
+const MONGO_PASSWORD=process.env.MONGO_PASSWORD;
+const MONGO_BASE_URL=process.env.MONGO_BASE_URL;
+const MONGO_URL=`mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_BASE_URL}/${DATABASE}?retryWrites=true&w=majority`
+
+// controllers
+
+const userController = require('./controller/userController')
+app.use('/user/api', userController)
+
+
+
+// connect to mongoose
+mongoose.connect(MONGO_URL).then(async()=>{
+    console.log('database connected')
+    // store test users
+    // await seedData();
+    app.listen(PORT, () => { console.log('listening on', PORT) });
+  })
