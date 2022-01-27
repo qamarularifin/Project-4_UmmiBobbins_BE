d7577f42613e60e3aad646015045a576bc7a81e6
qamarularifin <qamarulafro89@gmail.com>
2022-01-27 19:27:16 +0800

    com

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const app = express();
 const mongoose = require('mongoose')
 const cors = require("cors")
 require('dotenv').config();
-const CORS_WHITELIST = process.env.CORS_WHITELIST.split(',')
+//const CORS_WHITELIST = process.env.CORS_WHITELIST.split(',')
 
 
 
-//app.use(cors())
-app.use(cors({
-  origin: CORS_WHITELIST
-}));
+app.use(cors())
+// app.use(cors({
+//   origin: CORS_WHITELIST
+// }));
 app.use(express.json());
 
 
