f5459981302003013ceb6ee4277d04a2588ccfcf
qamarularifin <qamarulafro89@gmail.com>
2022-02-20 22:24:22 +0800

    changed back to server.listen

--- server.js
+++ server.js
@@ -68,17 +68,17 @@ app.use("/booking/api", bookingController);
 io.on("connection", (socket) => {
   console.log(`User connected: ${socket.id}`);
 
-  // //listen for frontend input of data which is room
-  // socket.on("join_room", (data) => {
-  //   socket.join(data);
-  //   console.log(`User with ID: ${socket.id} joined room: ${data}`);
-  // });
+  //listen for frontend input of data which is room
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
 
-  // //listens for data sent (socket.on) from frontend
-  // //and send data (socket.to) back to frontend
-  // socket.on("send_message", (data) => {
-  //   socket.to(data.room).emit("receive_message", data);
-  // });
+  //listens for data sent (socket.on) from frontend
+  //and send data (socket.to) back to frontend
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
 
   socket.on("disconnect", () => {
     console.log("User Disconnected", socket.id);
@@ -91,7 +91,7 @@ io.on("connection", (socket) => {
 // connect to mongoose
 mongoose.connect(MONGO_URL).then(async () => {
   console.log("database connected");
-  app.listen(PORT, () => {
+  server.listen(PORT, () => {
     console.log("listening on", PORT);
   });
 });
